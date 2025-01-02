0647290098a888549707eb121e318ac9fc71c489
Fernand Philip Lacuata <lacuatafernandphilip@gmail.com>
2025-01-02 18:37:44 +0800

    removed messagepack

--- RealTimeChatApp/obj/Release/net8.0/RealTimeChatApp.AssemblyInfo.cs
+++ RealTimeChatApp/obj/Release/net8.0/RealTimeChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimeChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f86cb5b80e6e79ce1c84451ce5fdd3d76814fcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimeChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimeChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
